7c6f9593e97a038eb3ff2766b7ba98c1faa1f986
Gago <gagik.simonyan@softconstruct.com>
2021-11-25 19:01:03 +0400

    first routing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ComposeMessageComponent } from "./compose-message/compose-message.component";
 
 const appRoutes: Routes = [
+  { path: 'compose', component: ComposeMessageComponent, outlet: 'popup' },
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
